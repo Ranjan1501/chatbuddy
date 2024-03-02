2f61d8f262b62134ee8c615bfc111b4bac307696
Ranjan1501 <ranjankumar1757@gmail.com>
2024-03-02 23:45:29 +0530

    Emit👂👂 Message from socket with timestamp and username data

---
+++ client/src/Component/Chat.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getDate() +
+          ":" +
+          parseInt(new Date().getMonth() + 1) +
+          ":" +
+          new Date(Date.now()).getFullYear() +
+          " " +
+          "at:" +
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData); 
+    }
+  };
+  return (
+    <div>
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body"></div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Type a message"
+          onChange={(e) => setCurrentMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>
+          <strong> ➡️</strong>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
