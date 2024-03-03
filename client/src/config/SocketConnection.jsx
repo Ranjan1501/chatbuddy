d064429d0ea879fb37c8c16c0c65683c2039013f
Ranjan1501 <ranjankumar1757@gmail.com>
2024-03-03 21:17:08 +0530

    Show both user chat📨 in chat window yours and others too

--- client/src/config/SocketConnection.jsx
+++ client/src/config/SocketConnection.jsx
@@ -3,36 +3,43 @@ import io from "socket.io-client";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Chat from "../Component/Chat";
-
+import "./SocketConnection.css";
 const socket = io.connect("http://localhost:4500");
 export function SocketConnection() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
   return (
-    <>
-      <h3> Welcome to bot🤖</h3>
-      <input
-        type="text"
-        placeholder="name.."
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3> Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="name.."
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
 
-      <input
-        type="text"
-        placeholder="Room Id"
-        onChange={(e) => {
-          setRoom(e.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>join chat</button>
-      <Chat socket={socket} username={username} room={room} />
-    </>
+          <input
+            type="text"
+            placeholder="Room Id"
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>join chat</button>
+        </div>
+      ) : (
+        <Chat key={socket.id} socket={socket} username={username} room={room} />
+      )}
+    </div>
   );
 }
